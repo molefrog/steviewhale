4b35c6cf4019a155634402dccd9afacf71ba4e0e
molefrog <molefrog@gmail.com>
2014-03-05 16:25:31 +0400

    Added Yandex.Metrika analytics

---
+++ app/utils/analytics/yandexMetrika.js
@@ -0,0 +1,24 @@
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+          w.yaCounter24199384 = new Ya.Metrika({
+            id: 24199384,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            trackHash: true
+          });
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2828,6 +2828,34 @@ module.exports = Storage = (function() {
 })();
 });
 
+;require.register("utils/analytics/yandexMetrika", function(exports, require, module) {
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+          w.yaCounter24199384 = new Ya.Metrika({
+            id: 24199384,
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            trackHash: true
+          });
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+
+});
+
 ;require.register("views/about/aboutView", function(exports, require, module) {
 var AboutView, View, _ref,
   __hasProp = {}.hasOwnProperty,
